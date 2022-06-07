d299319bd7c759333827140469aa6a06ed9cb92b
Rafael Miranda <35074501+rafamirandaa@users.noreply.github.com>
2022-06-07 16:47:48 -0300

    Requisitos 16, 17, 18 e 19

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const button = document.querySelector('#submitButton');
 const email = document.querySelector('#email');
 const password = document.querySelector('#password');
+const buttonSend = document.querySelector('#submit-btn');
+const checkbox = document.querySelector('#agreement');
 
 function login() {
   if (email.value === 'tryber@teste.com' && password.value === '123456') {
@@ -11,3 +13,13 @@ function login() {
 }
 
 button.addEventListener('click', login);
+
+function enableClick() {
+  if (checkbox.checked !== true) {
+    buttonSend.disabled = true;
+  } else {
+    buttonSend.disabled = false;
+  }
+}
+
+checkbox.addEventListener('click', enableClick);
