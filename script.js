0cae2ea58eb5915c2ad56cdf874a0355d68abd98
Rafael Miranda <35074501+rafamirandaa@users.noreply.github.com>
2022-06-07 19:09:51 -0300

    Requisito bonus 2 feito

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const button = document.querySelector('#submitButton');
-const email = document.querySelector('#email');
+const email = document.querySelector('#input-email');
 const password = document.querySelector('#password');
 const buttonSend = document.querySelector('#submit-btn');
 const checkbox = document.querySelector('#agreement');
@@ -33,3 +33,63 @@ function counterTextArea() {
 }
 
 caracteresTextArea.addEventListener('input', counterTextArea);
+
+// RECEBE
+const formUsername = document.querySelector('#formUsername');
+const formEmail = document.querySelector('#formEmail');
+const formHouse = document.querySelector('#formHouse');
+const formFamily = document.querySelector('#formFamily');
+const formMaterial = document.querySelector('#formMaterial');
+const formRating = document.querySelector('#formRating');
+const formObs = document.querySelector('#formObs');
+
+// ENVIA
+const inputName = document.querySelector('#input-name');
+const inputLastName = document.querySelector('#input-lastname');
+const house = document.querySelector('#house');
+const family = document.getElementsByName('family');
+const materials = document.querySelectorAll('.subject');
+const rating = document.getElementsByName('rate');
+const obs = document.querySelector('#textarea');
+
+function chooseFamily() {
+  for (let index = 0; index < family.length; index += 1) {
+    if (family[index].checked === true) {
+      return family[index].value;
+    }
+  }
+}
+
+function chooseRate() {
+  for (let index = 0; index < rating.length; index += 1) {
+    if (rating[index].checked === true) {
+      return rating[index].value;
+    }
+  }
+}
+
+function chooseMaterial() {
+  const array = [];
+  for (let index = 0; index < materials.length; index += 1) {
+    if (materials[index].checked === true) {
+      array.push(materials[index].value);
+    }
+  }
+  return array.join(', ');
+}
+
+const form = document.querySelector('#evaluation-form');
+
+function sendData(event) {
+  event.preventDefault();
+  formUsername.innerText = `Nome: ${inputName.value} ${inputLastName.value}`;
+  formEmail.innerText = `Email: ${email.value}`;
+  formHouse.innerText = `Casa: ${house.value}`;
+  formObs.innerText = `Observações: ${obs.value}`;
+  formFamily.innerText = `Família: ${chooseFamily()}`;
+  formMaterial.innerText = `Matérias: ${chooseMaterial()}`;
+  formRating.innerText = `Avaliação: ${chooseRate()}`;
+  form.style.display = 'none';
+}
+
+buttonSend.addEventListener('click', sendData);
